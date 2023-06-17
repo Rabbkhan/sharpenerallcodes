e4735850780344c400bdd1ade014b1e741b451a9
Rabbil khan <rabbilkhan125@gmail.com>
2023-06-17 23:24:14 +0530

    adding routing code using node js

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require("http");
-
 const server =http.createServer((req,res)=>{
-    res.end("Rabbil khan")
-console.log("Rabbil khan");
+    const url = req.url;
+//     res.end("Rabbil khan")
+// console.log("Rabbil khan");
+
+if (url === "/"){
+    res.end("Welcome home");
+}
+if (url === "/about"){
+    res.end("Welcome to About us page");
+}
+if (url === "/node"){
+    res.end("Welcome to node js project");
+}
+
 } )
+
 server.listen(4000)
