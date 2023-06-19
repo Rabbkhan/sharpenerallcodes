308c0e841b69f1bcb936b02fd9c20bde3331dc6e
Rabbil khan <rabbilkhan125@gmail.com>
2023-06-19 20:36:15 +0530

    storing message in message text file

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const http = require("http");
+const fs = require('fs');
 const server =http.createServer((req,res)=>{
     const url = req.url;
-//     res.end("Rabbil khan")
-// console.log("Rabbil khan");
+    const method = req.method;
 
 if (url === "/"){
-    res.end("Welcome home");
-}
-if (url === "/about"){
-    res.end("Welcome to About us page");
-}
-if (url === "/node"){
-    res.end("Welcome to node js project");
-}
+    res.write('<html>');
+    res.write('<head><title> Enter Message</title> </head>');
+    res.write('<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit">send</button></form> </body>');
+    res.write('<html>');
+    res.write('<html>');
 
+    return res.end();
+}
+if(url === '/message' && method ==='POST'){
+fs.writeFileSync('message.txt', 'Hello bro whatsup');
+res.statusCode=302;
+res.setHeader('location','/');
+return res.end();
+}
 } )
 
 server.listen(4000)
