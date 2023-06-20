1d22168a33378e819c416779865a5ffbe5e06807
Rabbil khan <rabbilkhan125@gmail.com>
2023-06-20 13:45:38 +0530

    adding clean code

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-const http = require("http");
-const fs = require('fs');
-const server =http.createServer((req,res)=>{
-    const url = req.url;
-    const method = req.method;
+const http = require('http');
 
-if (url === "/"){
-    res.write('<html>');
-    res.write('<head><title> Enter Message</title> </head>');
-    res.write('<body><form action="/message" method="POST"> <input type="text" name="message"><button type="submit">send</button></form> </body>');
-    res.write('<html>');
-    res.write('<html>');
+const routes = require('./routes');
 
-    return res.end();
-}
-if(url === '/message' && method ==='POST'){
-fs.writeFileSync('message.txt', 'Hello bro whatsup');
-res.statusCode=302;
-res.setHeader('location','/');
-return res.end();
-}
-} )
+console.log(routes.someText);
 
-server.listen(4000)
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
