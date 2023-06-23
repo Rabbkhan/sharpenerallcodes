f57b6808702f04f8f3df47857df0d4ad8e1e7b0b
Rabbil khan <rabbilkhan125@gmail.com>
2023-06-24 01:43:12 +0530

    push the routers clean code

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const bodyParser= require('body-parser')
 const app= express();
+const addminrouter =require('./routes/admin')
+const shoprouter =require('./routes/shop')
 app.use(bodyParser.urlencoded({extended: false}));
 PORT = 4000;
 
- app.use('/add-product', (req, res, next)=>{
-res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add product</button>')
+app.use('/admin',addminrouter);
+app.use(shoprouter);
+app.use((req,res,next)=>{
+res.send('<h1>404 No Page found</h1>')
+})
 
- });
-
- app.use('/product', (req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
- })
-
- app.use('/', (req, res, next)=>{
-res.send(`<h1> hello sharpener</h1>`)
- });
+ 
 
 app.listen(`${PORT}`);
