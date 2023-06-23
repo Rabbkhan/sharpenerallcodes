495d026a64dbb4b495448245c8544911f552b376
Rabbil khan <rabbilkhan125@gmail.com>
2023-06-24 01:08:38 +0530

    adding product page with data console print

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const http = require('http');
 const express = require('express');
+const bodyParser= require('body-parser')
 const app= express();
+app.use(bodyParser.urlencoded({extended: false}));
 PORT = 4000;
-// console.log(routes.someText);
+
+ app.use('/add-product', (req, res, next)=>{
+res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button>')
+
+ });
+
+ app.use('/product', (req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+ })
+
  app.use('/', (req, res, next)=>{
 res.send(`<h1> hello sharpener</h1>`)
- })
+ });
 
 app.listen(`${PORT}`);
